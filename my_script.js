200008f3a6c387de1ef039279593139170bbe20e
Ciarm-nik <nikcia@libero.it>
2021-04-19 17:24:33 +0200

    Modifiche stile css

--- my_script.js
+++ my_script.js
@@ -13,52 +13,52 @@
 var bottomGenerate = document.getElementById("genera");
 bottomGenerate.addEventListener("click",
 
-function(event) {
-    event.preventDefault()
+    function (event) {
+        event.preventDefault()
 
-    var nomePasseggero = document.getElementById("name").value;
-    document.getElementById("nomePasseggero").innerHTML = nomePasseggero;
+        var nomePasseggero = document.getElementById("name").value;
+        document.getElementById("nomePasseggero").innerHTML = nomePasseggero;
 
-    var wagonNumber = Math.floor(Math.random() * 9) + 1;
-    document.getElementById("carrozza").innerHTML = wagonNumber;
+        var wagonNumber = Math.floor(Math.random() * 9) + 1;
+        document.getElementById("carrozza").innerHTML = wagonNumber;
 
-    var cpCode = Math.floor(Math.random() * 10000) + 90000;
-    document.getElementById("codice_cp").innerHTML = cpCode;
+        var cpCode = Math.floor(Math.random() * 10000) + 90000;
+        document.getElementById("codice_cp").innerHTML = cpCode;
 
-    var km = document.getElementById("km").value;
+        var km = document.getElementById("km").value;
 
-    var age = document.getElementById("fascia-eta").value;
+        var age = document.getElementById("fascia-eta").value;
 
-    // Calcolo prezzo del biglietto
+        // Calcolo prezzo del biglietto
 
-    var prezzoAlKilometro = 0.21;
+        var prezzoAlKilometro = 0.21;
 
-    var prezzoBigliettoSenzaSconto = prezzoAlKilometro * km
+        var prezzoBigliettoSenzaSconto = prezzoAlKilometro * km
 
-    var offerta = "Tariffa Standard"
+        var offerta = "Tariffa Standard"
 
-    var discount;
+        var discount;
 
-    if (age == "minorenne") {
-        discount = prezzoBigliettoSenzaSconto *0.20;
-        offerta = "Sconto Minorenne"
-    } else if (age == "over65") {
-        discount = prezzoBigliettoSenzaSconto *0.40;
-        offerta = "Sconto over 65";
-    } else {
-        discount = 0
-         prezzoBigliettoSenzaSconto;
+        if (age == "minorenne") {
+            discount = prezzoBigliettoSenzaSconto * 0.20;
+            offerta = "Sconto Minorenne"
+        } else if (age == "over65") {
+            discount = prezzoBigliettoSenzaSconto * 0.40;
+            offerta = "Sconto over 65";
+        } else {
+            discount = 0
+            prezzoBigliettoSenzaSconto;
+        }
+
+        var prezzoFinale = prezzoBigliettoSenzaSconto - discount
+
+        document.getElementById("costo-biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
+
+        document.getElementById("offerta").innerHTML = offerta;
+
+        // Animazione sezione ticket
+        var biglietto = document.getElementById("container_bottom");
+        biglietto.classList.remove("hidden");
+        biglietto.classList.add("show");
     }
-
-    var prezzoFinale = prezzoBigliettoSenzaSconto - discount
-
-    document.getElementById("costo-biglietto").innerHTML = prezzoFinale.toFixed(2) + " €";
-
-    document.getElementById("offerta").innerHTML = offerta;
-
-    // Animazione sezione ticket
-    var biglietto = document.getElementById("container_bottom");
-    biglietto.classList.remove("hidden");
-    biglietto.classList.add("show");
-}
 )
